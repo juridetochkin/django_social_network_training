2e4930ae76b796b9d2e8e346e54fe1d717492478
jerk <detochkin.j@gmail.com>
2021-03-28 12:22:42 +0300

    Fixed (hardcoded flatpages routing issue)

--- yatube/urls.py
+++ yatube/urls.py
@@ -5,18 +5,23 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("about/", include('django.contrib.flatpages.urls')),
+    path("about/", include("django.contrib.flatpages.urls")),
     path("auth/", include("users.urls")),
     path("auth/", include("django.contrib.auth.urls")),
     path('__debug__/', include(debug_toolbar.urls)),
-    path("", include("posts.urls")),
     ]
 
 
 # Flatpages
 urlpatterns += [
-        path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
-        path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
-        path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about-author'),
-        path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='about-spec')
-]
+    path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
+    path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
+    path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about-author'),
+    path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='about-spec')
+    ]
+
+
+# Posts App Urls
+urlpatterns += [
+    path("", include("posts.urls")),
+    ]
